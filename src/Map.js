d3c01654b97918c2f77fad571ca526baa56cfaaf
Sean Park <spark3@andrew.cmu.edu>
2018-10-31 00:04:07 -0400

    removed placeholder content

---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="Map">
+        map
+      </div>
+    );
+  }
+}
+
+export default Map;
