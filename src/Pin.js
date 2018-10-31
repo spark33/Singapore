334af22c36321d6e0d218801c6f0dc49cea62f0a
Sean Park <spark3@andrew.cmu.edu>
2018-10-31 00:48:21 -0400

    styled pin elements

--- src/Pin.js
+++ src/Pin.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Pin extends Component {
+
+  constructor(props) {
+    super(props);
+    this.styles = {
+      top: props.y + '%',
+      left: props.x + '%',
+    };
+  }
+
   render() {
     return (
-      <div className="Pin">
-        pin
+      <div className="Pin" style={ this.styles }>
+        <img src={ require("./map-pin.png") } />
+        <h5>{ this.props.name }</h5>
       </div>
     );
   }
